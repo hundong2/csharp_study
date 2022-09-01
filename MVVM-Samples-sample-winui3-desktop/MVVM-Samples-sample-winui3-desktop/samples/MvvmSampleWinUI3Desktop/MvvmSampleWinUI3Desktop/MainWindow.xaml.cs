547c1299453a61b7256cf434bb7c75a786d57dc2
hundong2 <ehdgns104@gmail.com>
2022-09-01 10:25:30 +0900

    MVVM sample added

---
+++ MVVM-Samples-sample-winui3-desktop/MVVM-Samples-sample-winui3-desktop/samples/MvvmSampleWinUI3Desktop/MvvmSampleWinUI3Desktop/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.UI.Xaml;
+
+namespace MvvmSampleWinUI3Desktop
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            this.Title = "MVVM Toolkit sample app";
+            this.ExtendsContentIntoTitleBar = true;
+
+            Shell.SetTitleBar(this);
+        }
+    }
+}
