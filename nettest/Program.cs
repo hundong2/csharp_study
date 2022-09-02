db918968b294bc2060769f5b03940ead8f563600
donghun lee <ehdgns104@gmail.com>
2022-09-02 00:23:49 +0000

    C#11 test project added

---
+++ nettest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace nettest
+{
+    [TypeAttribute(typeof(string))]
+    public string Method() => default;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
