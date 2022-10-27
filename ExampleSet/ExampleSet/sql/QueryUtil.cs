e6a2a53de1bf81c021d1206ba52828b1b7e6e19c
hundong2 <ehdgns104@gmail.com>
2022-10-27 13:52:58 +0900

    default folder added

---
+++ ExampleSet/ExampleSet/sql/QueryUtil.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ExampleSet.sql
+{
+    class QueryUtil
+    {
+    }
+}
