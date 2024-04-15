3876862c6b02622243eaa5a17c7a09aef2ff43f2
hundong2 <ehdgns104@gmail.com>
2024-04-15 22:29:36 +0900

    Add using function encapsulation

--- dailyStudy/SolutionDailyApril/project1/Program.cs
+++ dailyStudy/SolutionDailyApril/project1/Program.cs
@@ -9,7 +9,7 @@ using UtilityLibrary;
             var stringVariable = stringLib.GetInstance("hello world!!");
             Console.WriteLine(stringVariable.GetStringValue());
 
-            
+
             var exampleAdvance = exampleAdvancedTrick.GetInstance();
             var value = exampleAdvance.GetCalculate(10,5);
 
@@ -17,5 +17,19 @@ using UtilityLibrary;
             var examplePattern = new ExamplePatternMatching();
             examplePattern.TestPatternMatching(examplePattern._circle);
             examplePattern.TestPatternMatching(examplePattern._square);
+
+            //Local functions for encapsulation
+            var _fibonacci = Fibonacci(5);
+        }
+
+        /// <summary>
+        /// Local functions for encapsulation
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        static public IEnumerable<int> Fibonacci(int n)
+        {
+            int Fib(int term) => term <= 2 ? 1 : Fib(term - 1) + Fib(term - 2);
+            return Enumerable.Range(1, n).Select(Fib);
         }
     }
