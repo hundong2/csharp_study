d07d60ee8d9522e8fba1dbc9c32de5bde391d56e
hundong2 <ehdgns104@gmail.com>
2024-04-15 22:25:35 +0900

    Add Pattern Matching example

--- dailyStudy/SolutionDailyApril/project1/Program.cs
+++ dailyStudy/SolutionDailyApril/project1/Program.cs
@@ -9,7 +9,13 @@ using UtilityLibrary;
             var stringVariable = stringLib.GetInstance("hello world!!");
             Console.WriteLine(stringVariable.GetStringValue());
 
+            
             var exampleAdvance = exampleAdvancedTrick.GetInstance();
             var value = exampleAdvance.GetCalculate(10,5);
+
+            //Example Pattern Matching 
+            var examplePattern = new ExamplePatternMatching();
+            examplePattern.TestPatternMatching(examplePattern._circle);
+            examplePattern.TestPatternMatching(examplePattern._square);
         }
     }
---
+++ dailyStudy/SolutionDailyApril/stringLib/ExamplePatternMatching.cs
@@ -0,0 +1,47 @@
+﻿namespace UtilityLibrary;
+
+public class ExamplePatternMatching
+{
+    public class Circle
+    {
+        public string name = "circle";
+        public int x;
+        public int y;
+        public double radius;
+    }
+    public class Square
+    {
+        public string name = "square";
+        public int x;
+        public int y;
+        public double side;
+    }
+    public Circle _circle = new Circle(){
+        radius = 1.1,
+        x = 0,
+        y = 1
+    };
+    public Square _square = new Square(){
+        side = 1.3,
+        x = 1,
+        y = 2
+    };
+
+    public ExamplePatternMatching()
+    {
+
+    }    
+
+
+    public void TestPatternMatching(object value)
+    {
+        if( value is Circle c )
+        {
+            Console.WriteLine($@"name {c.name}, {c.radius}");
+        }
+        if(value is Square s)
+        {
+            Console.WriteLine($@"name {s.name}, {s.side}");
+        }
+    }
+}
