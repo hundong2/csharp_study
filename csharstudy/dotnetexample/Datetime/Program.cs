220869047846b6b0272898d7c1e5728dcf7ef0a7
hundong2 <ehdgns104@gmail.com>
2024-08-05 23:29:10 +0900

    Update System.Diagnostics.Stopwatch

--- csharstudy/dotnetexample/Datetime/Program.cs
+++ csharstudy/dotnetexample/Datetime/Program.cs
@@ -2,6 +2,15 @@
 
 class Program
 {
+    public static long Sum()
+    {
+        long sum = 0;
+        for(int i = 0; i < 1000000; i++)
+        {
+            sum += i;
+        }
+        return sum;
+    }    
     static void Example1()
     {
         DateTime now =  DateTime.Now;
@@ -16,9 +25,39 @@ class Program
         worldcup2002 = new DateTime(2002, 5, 31, 0, 0, 0, DateTimeKind.Local);
         Console.WriteLine($"{worldcup2002} : {worldcup2002.Kind}");
     }
+    //TimeSpan
+    static void Example2()
+    {
+        DateTime endOfYear = new DateTime(DateTime.Now.Year, 12, 31);
+        DateTime now = DateTime.Now;
+
+        Console.WriteLine($"today's date: {now}");
+        TimeSpan gap = endOfYear - now;
+        Console.WriteLine($"remain date of this year : {gap.TotalDays}"); 
+        //TotalHours, TotalMilliseconds, TotalMinutes, TotalSeconds 
+    }
+
+    //System.Diagnostics.Stopwatch 
+    static void Example3()
+    {
+        System.Diagnostics.Stopwatch st = new System.Diagnostics.Stopwatch();
+
+        st.Start();
+        Sum();
+        st.Stop();
+
+        Console.WriteLine($"Total Ticks: {st.ElapsedTicks}");
+        Console.WriteLine($"Millisecond: {st.ElapsedMilliseconds}");
+        Console.WriteLine($"Second: {st.ElapsedMilliseconds / 1000}"); //millisecond to second 
+
+        //Stop.Frequency attribute 
+        Console.WriteLine($"Second : {st.ElapsedTicks/System.Diagnostics.Stopwatch.Frequency}");
+    }
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
         Example1();
+        Example2();
+        Example3();
     }
 }
