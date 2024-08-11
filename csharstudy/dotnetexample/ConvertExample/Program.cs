ffb0a03b8a309cd3f24a815b60409ba0d20bffd9
hundong2 <ehdgns104@gmail.com>
2024-08-11 22:47:34 +0900

    Update Stream Writer, Reader Example

--- csharstudy/dotnetexample/ConvertExample/Program.cs
+++ csharstudy/dotnetexample/ConvertExample/Program.cs
@@ -30,10 +30,86 @@ class Program
         Console.WriteLine(result); 
         Console.WriteLine($"string result {resultStr}");
     }
+    static void ExampleMemoryStream()
+    {
+        byte[] shortBytes = BitConverter.GetBytes((short)32000);
+        byte[] intBytes = BitConverter.GetBytes(1652300);
+
+        MemoryStream ms = new MemoryStream();
+        ms.Write(shortBytes, 0, shortBytes.Length);
+        ms.Write(intBytes, 0, intBytes.Length);
+
+        ms.Position = 0;
+
+        //short data read from MemoryStream ms
+        byte[] outBytes = new byte[2];
+        ms.Read(outBytes, 0,2);
+        int shortResult = BitConverter.ToInt16(outBytes, 0);
+        Console.WriteLine(shortResult);
+
+        outBytes = new byte[4];
+        ms.Read(outBytes, 0, 4);
+        int intResult = BitConverter.ToInt32(outBytes, 0);
+        Console.WriteLine(intResult);
+    }
+    static void ExampleMemoryStream2()
+    {
+        Console.WriteLine("Memory Stream Example : Using ToArray of MemoryStream");
+        byte[] shortBytes = BitConverter.GetBytes((short)32000);
+        byte[] intBytes = BitConverter.GetBytes(1652300);
+
+        MemoryStream ms = new MemoryStream();
+        ms.Write(shortBytes, 0, shortBytes.Length);
+        ms.Write(intBytes, 0, intBytes.Length);
+
+        byte[] buf = ms.ToArray();
+
+        int shortResult = BitConverter.ToInt16(buf, 0);
+        Console.WriteLine(shortResult);
+
+        int intResult = BitConverter.ToInt32(buf, 2);
+        Console.WriteLine(intResult);
+    }
+    static void ExampleStreamEncoding()
+    {
+        Console.WriteLine("ExampleStreamEncoding");
+        //Using Encoding 
+        MemoryStream ms = new MemoryStream();
+        byte[] buf = System.Text.Encoding.UTF8.GetBytes("Hello World");
+        ms.Write(buf, 0, buf.Length);
+        ms.Position = 0;
+        StreamReader sr = new StreamReader(ms, System.Text.Encoding.UTF8);
+        string txt = sr.ReadToEnd();
+        Console.WriteLine(txt); //Hello World
+    }
+    static MemoryStream ExampleStreamWriter()
+    {
+        Console.WriteLine("StreamWriter to StreamReader");
+        MemoryStream ms = new MemoryStream();
+        StreamWriter sw = new StreamWriter(ms, System.Text.Encoding.UTF8);
+        sw.WriteLine("Hello world");
+        sw.WriteLine("Anderson");
+        sw.Write("32000");
+        sw.Flush();
+
+        return ms;
+    }
+    static void ExampleStreamReader(MemoryStream ms)
+    {
+        ms.Position = 0;
+
+        StreamReader sr = new StreamReader(ms, System.Text.Encoding.UTF8);
+        string txt = sr.ReadToEnd();
+        Console.WriteLine(txt);
+    }
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
         ExampleBitConvert(); //little endian
         ExampleBitConvert2();
+        ExampleMemoryStream();
+        ExampleMemoryStream2();
+        ExampleStreamEncoding();
+        ExampleStreamReader(ExampleStreamWriter());
     }
 }
